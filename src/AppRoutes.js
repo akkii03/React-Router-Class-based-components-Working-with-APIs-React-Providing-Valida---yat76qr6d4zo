2f6621c3a985b5e204f8996197b6a57eb6a4ca05
akkii03 <akkiindoria72@gmail.com>
2022-07-19 17:06:02 -1200

    need more logic

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React,{Routes,Route} from 'react';
+import { Home } from './Pages/Home';
+import { Index } from './Pages/Index';
+import { NotFound } from './Pages/NotFound';
 
 export const AppRoutes = () => {
     return (
-
+        <>
+        <Routes>
+            <Route path="/" element={<Index/>}/>
+            <Route path="/home" element={<Home/>}/>
+            <Route element={<NotFound/>}/>
+        </Routes>
+        </>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 export const NavBar = () =>{
     return(
-
+        <nav> 
+            <ul>
+                <li>
+                    <Link className="index-link" to="/">Index</Link>
+                    <Link className='home-link' to="/home">Home</Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
